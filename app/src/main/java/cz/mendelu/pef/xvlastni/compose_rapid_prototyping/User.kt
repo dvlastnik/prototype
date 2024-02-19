4714f34ba7f0eb33a1073f258ed5c96f8bd921df
xvlastni <xvlastni@mendelu.cz>
2024-02-19 22:56:35 +0100

    accessing data class variables

--- app/src/main/java/cz/mendelu/pef/xvlastni/compose_rapid_prototyping/User.kt
+++ app/src/main/java/cz/mendelu/pef/xvlastni/compose_rapid_prototyping/User.kt
@@ -3,4 +3,7 @@ package cz.mendelu.pef.xvlastni.compose_rapid_prototyping
 import cz.mendelu.pef.xvlastni.prototype.RapidPrototype
 
 @RapidPrototype
-class ApiResponse
+data class User(
+    val id: Long,
+    val username: String
+)
--- prototype/src/main/java/cz/mendelu/pef/xvlastni/prototype/RapidPrototypeProcessor.kt
+++ prototype/src/main/java/cz/mendelu/pef/xvlastni/prototype/RapidPrototypeProcessor.kt
@@ -10,6 +10,7 @@ import com.google.devtools.ksp.validate
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.Import
 
 class RapidPrototypeProcessor(private val codeGenerator: CodeGenerator) : SymbolProcessor {
     override fun process(resolver: Resolver): List<KSAnnotated> {
@@ -20,18 +21,38 @@ class RapidPrototypeProcessor(private val codeGenerator: CodeGenerator) : Symbol
         symbols.forEach { symbol ->
             if (!symbol.validate()) return@forEach
 
+            // variables of the data class
+            val properties = symbol.getAllProperties()
+
+            // variables for naming
             val packageName = symbol.packageName.asString()
             val className = symbol.simpleName.asString()
             val fileName = "${className}Screen"
 
+            // variables for used jetpack compose classes
+            val composableClass = ClassName("androidx.compose.runtime", "Composable")
+            val columnClass = ClassName("androidx.compose.foundation.layout", "Column")
+            val textClass = ClassName("androidx.compose.material3", "Text")
+            val modifierClass = ClassName("androidx.compose.ui", "Modifier")
+
+            // making composable function
+            val funSpecBuilder = FunSpec.builder(fileName)
+                .addAnnotation(composableClass)
+                .addParameter("user", ClassName(packageName, className))
+                .beginControlFlow("%T(modifier = %T.fillMaxSize())", columnClass, modifierClass)
+
+            properties.forEach { property ->
+                val propertyName = property.simpleName.asString()
+                funSpecBuilder
+                    .addStatement("%T(text = %S + user.%L)", textClass, "$propertyName: ", propertyName)
+            }
+
+            // end of the composable function
+            funSpecBuilder.endControlFlow()
+
             val fileSpec = FileSpec.builder(packageName, fileName)
-                //.addImport(ClassName("androidx.compose.material3", "Text"), "")
-                .addFunction(
-                    FunSpec.builder(fileName)
-                        .addAnnotation(ClassName("androidx.compose.runtime", "Composable"))
-                        .addStatement("%T(text = %S)", ClassName("androidx.compose.material3", "Text"), "Generated $className")
-                        .build()
-                )
+                .addImport("androidx.compose.foundation.layout.fillMaxSize", "")
+                .addFunction(funSpecBuilder.build())
                 .build()
 
             val file = codeGenerator.createNewFile(
